ae13664dbaa5981a0fd4037bd28d7ce6691b17f9
Amanda Zanata <amandazanata46@gmail.com>
2022-11-16 19:26:37 -0300

    exporta função fetch para main.js / inicia função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,11 @@
+const produtos = document.querySelector('.products');
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = (query) => {
   // seu código aqui
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  then((resposta) => resposta.json());
 };
