a792a6418ad8df7a02a91ff1bf1eaebbd66141b7
Amanda Zanata <amandazanata46@gmail.com>
2022-11-18 17:17:08 -0300

    adiciona alterações main.js requisito 4

--- src/main.js
+++ src/main.js
@@ -6,17 +6,36 @@ import './style.css';
 const sectionProduct = document.querySelector('.products');
 const sectionContainer = document.querySelector('.container');
 
-async function aguardaCarregar() {
+/* function aguardaCarregar() {
   const criaElemento = document.createElement('h2');
   criaElemento.innerHTML = 'carregando...';
   criaElemento.className = ('loading');
+  criaElemento.style.marginLeft = '5px';
   sectionContainer.appendChild(criaElemento);
+  return criaElemento;
 }
+
+function removeCarregando() {
+  const pegaElemento = document.querySelector('.loading');
+  pegaElemento.classList.remove('loading');
+  return pegaElemento;
+} */
 
 const listaDeProdutos = await fetchProductsList('computador');
 listaDeProdutos.forEach((product) => sectionProduct
   .appendChild(createProductElement(product)));
 
-aguardaCarregar();
-
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+/* window.onload = function carregaPagina() {
+  if (listaDeProdutos === true) {
+    return listaDeProdutos;
+  }
+  if (listaDeProdutos === false) {
+    try {
+      aguardaCarregar();
+    } catch (e) {
+      removeCarregando();
+    }
+  }
+}; */
