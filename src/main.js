224286d0ccaa06cb513146f2f1c67f1addea1334
Amanda Zanata <amandazanata46@gmail.com>
2022-11-21 16:29:09 -0300

    carregando sumindo conforme esperado

--- src/main.js
+++ src/main.js
@@ -3,56 +3,49 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-document.querySelector('.cep-button').addEventListener('click', searchCep);
+/* document.querySelector('.cep-button').addEventListener('click', searchCep); */
 
 // variáveis globais
 const sectionProduct = document.querySelector('.products');
-const sectionContainer = document.querySelector('.container');
-const paragrafo = document.createElement('p');
+const criaParagrafo = document.createElement('p');
+const criaH1 = document.createElement('h1');
 
-// Cria elemento e adiciona a classe loading + texto com aviso de carregando...
+// Requisito 4 - Cria elemento e adiciona a classe loading + texto com aviso de carregando...
 const aguardaCarregar = () => {
-  paragrafo.classList.add = 'loading';
-  paragrafo.textContent = 'carregando...';
+  criaParagrafo.classList.add = 'loading';
+  criaParagrafo.innerText = 'carregando...';
 
-  sectionProduct.appendChild(paragrafo);
+  sectionProduct.appendChild(criaParagrafo);
+};
 
-  return sectionProduct;
+// Requisito 5 - Função para gerar lista de produtos e erro de requisição de API
+const trataErro5 = () => {
+  criaH1.classList.add = 'error';
+  criaH1.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  sectionProduct.appendChild(criaH1);
 };
 
 // Requisito 4 - Remove elemento com a classe loading
 const removeParagrafo = () => {
-  paragrafo.remove();
-};
-
-// Requisitos 1 e 5 - Função para gerar lista de produtos e erro de requisição de API
-const trataErro5 = () => {
-  const elemento = document.createElement('p');
-  elemento.classList.add = 'error';
-  elemento.textContent = 'Algum erro ocorreu, recarregue a página e tente novamente';
-
-  sectionContainer.appendChild(elemento);
+  criaParagrafo.remove();
 };
 
 const trataAPI = async () => {
   aguardaCarregar();
   try {
     const listaDeProdutos = await fetchProductsList('computador');
-    listaDeProdutos.forEach((product) => { // requisito 1
-      const produtcts = createProductElement(product);
-      sectionProduct.appendChild(produtcts);
-      removeParagrafo();
+    removeParagrafo();
+    listaDeProdutos.forEach((product) => { // requisito 3 - Gera lista de produtos 'computador'
+      const products = createProductElement(product);
+      sectionProduct.appendChild(products);
     });
   } catch (error) {
-    console.log('erro');
-    removeParagrafo();
     trataErro5();
+    removeParagrafo();
   }
 };
 trataAPI();
-
-/* window.onload = () => {
+/*
+window.onload = () => {
   trataAPI();
-};
-
-console.log(await fetchProductsList('computador')); */
+}; */
