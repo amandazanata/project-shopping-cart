a80ab5dd94d10fd0a7bf31a6b21924b84e459eef
Amanda Zanata <amandazanata46@gmail.com>
2022-11-21 16:33:04 -0300

    corrige linter 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { searchCep } from './helpers/cepFunctions';
+/* import { searchCep } from './helpers/cepFunctions'; */
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
